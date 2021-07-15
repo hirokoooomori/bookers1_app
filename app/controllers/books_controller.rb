fcf84b0232542c615322947a0a53e072565290dd
hirokoooomori <hirouta0408@gmail.com>
2021-07-15 06:56:43 +0000

    [File]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,25 @@
 class BooksController < ApplicationController
   
+  def index
+    @book = Book.new
+  end
+  
   def top
+  end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+  end
+  
+   private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
   
 end
